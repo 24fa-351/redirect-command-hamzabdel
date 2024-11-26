6b91b464d3fc087d0dd547ed23b824a9710f92db
hamzabdel <hamzaabdel094@gmail.com>
2024-11-25 16:03:58 -0800

    added clang file needs work

--- main.c
+++ main.c
@@ -1,32 +1,37 @@
 #include <stdio.h>
 
-void parse_cmd(char *cmd, char **argv){
+void parse_cmd(char *cmd, char **argv)
+{
     char *token = strtok(cmd, " ");
     int i = 0;
 }
 
-void redirect(int fd, int new_fd){
-    if (fd != new_fd){
+void redirect(int fd, int new_fd)
+{
+    if (fd != new_fd)
+    {
         dup2(fd, new_fd);
         close(fd);
     }
 }
 
-int main(int argc, int argv[]) {
+int main(int argc, int argv[])
+{
     char *inp = argv[1];
     char *cmd = argv[2];
     char *out = argv[3];
 
     parse_cmd(cmd);
 
-    if(pid == 0){
-        if(strcmp(inp, "-") != 0){
+    if (pid == 0)
+    {
+        if (strcmp(inp, "-") != 0)
+        {
             int inp_fd = open(inp, O_RDONLY);
-            
+
             redirect(inp_fd, STDIN_FILENO);
         }
     }
-
 
     return 0;
 }
